95a55d7c83cdec5a5545dcc092a8f4f221d4253f
Quynh anh <71160175+Quynhanh4622@users.noreply.github.com>
2021-05-31 21:41:05 +0700

    assignment

---
+++ T2009M_/assignment/Bai1/Clothing.cs
@@ -0,0 +1,9 @@
+﻿namespace T2009M_.assignment.Bai1
+{
+    public abstract class Clothing
+    {
+        public abstract void Sales();
+        public abstract void Materials();
+        public abstract void CommonWear();
+    }
+}
---
+++ T2009M_/assignment/Bai1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace T2009M_.assignment.Bai1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TrendSetting ts = new TrendSetting();
+            ts.Sales();
+            ts.Materials();
+            Console.WriteLine("------------------------");
+
+            ClimateControl cc = new ClimateControl();
+            cc.Sales();
+            cc.Materials();
+            Console.WriteLine("-------------------------");
+
+            Sports s = new Sports();
+            s.Sales();
+            s.Materials();
+        }
+    }
+}
