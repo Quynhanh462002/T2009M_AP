ae830eeb15f1c9cc6f7739b23d97e1deee22b027
Quynh anh <71160175+Quynhanh4622@users.noreply.github.com>
2021-05-31 08:28:11 +0700

    lab5

---
+++ T2009M_/Lab5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace T2009M_.Lab5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Set Min");
+            int min = int.Parse(Console.ReadLine() ?? string.Empty);
+            Console.WriteLine("Set Sec");
+            int sec = int.Parse(Console.ReadLine() ?? string.Empty);
+
+            while (min >= 0)
+            {
+                while (sec >= 0)
+                {
+                    Console.WriteLine(min + ":" + sec);
+                    sec--;
+                    try
+                    {
+                        Thread.Sleep(1000);
+                    }
+                    catch (Exception e)
+                    {
+                    }
+                }
+                if (sec < 0)
+                {
+                    sec = 59;
+                }
+
+                min--;
+            }
+            Console.Write("Bumm...");
+            
+        }
+    }
+}
